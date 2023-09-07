859300e783051b594c494a6aa1a94a08c88482a3
Hansol Lee <gksthf996@naver.com>
2023-09-07 17:25:37 +0900

    book 뷰추가, 템플릿 추가

--- books/urls.py
+++ books/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from books import views
 
 urlpatterns = [
+    path('', views.BookListView.as_view(), name='book_list'),
     path('my_rentals/', views.ManageBookListView.as_view(), name='manage_book_list'),
+    # path('books/<book_id>/', views.BookDetailView.as_view(), name='book_detail'),
     ]
--- books/views.py
+++ books/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+from books.models import Item
+from users.forms import BookRentalForm
+
+
+class BookListView(ListView):
+    def get(self, request):
+        items = Item.objects.all()
+        from django.shortcuts import render
+        return render(request, 'books/book/list.html', {'items': items})
+
+
+class ManageBookListView(ListView):
+    template_name = 'books/manage/list.html'
+
+
+# class BookDetailView(DetailView):
+#     model = Item
+#     template_name = 'books/book/detail.html'
+#
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['rental_form'] = BookRentalForm(initial={'item': self.object})
+#         return context
