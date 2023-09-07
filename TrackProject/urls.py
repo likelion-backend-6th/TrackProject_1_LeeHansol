ab977a59788338393882a74f8cc7b6ec783c040e
Hansol Lee <gksthf996@naver.com>
2023-09-07 17:25:58 +0900

    urlpatterns 추가

--- TrackProject/urls.py
+++ TrackProject/urls.py
@@ -20,12 +20,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
+from books import views
+
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('admin/', admin.site.urls),
-    path('books/', include('books.urls')),
-    path('', BookListView.as_view(), name='book_list'),
+    # path('books/', include('books.urls')),
+    path('users/', include('users.urls')),
+    path('', include('books.urls'), name='book_list'),
+
 ]
 
 if settings.DEBUG:
